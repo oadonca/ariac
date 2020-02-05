20bf787e52278abd4eaff5a7fef1deb8628b1e92
Gent Semaj <gentsemaj@live.com>
2020-02-05 00:31:34 -0500

    Restructure (again)

---
+++ workspace/build/universal_robot/ur_msgs/CMakeFiles/ur_msgs_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ur_msgs_generate_messages_lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/ToolDataMsg.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/Digital.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/IOStates.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/RobotModeDataMsg.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/Analog.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/MasterboardDataMsg.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/msg/RobotStateRTMsg.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/srv/SetPayload.lisp"
+  "/home/gents/ariac/devel/share/common-lisp/ros/ur_msgs/srv/SetIO.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ur_msgs_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ workspace/build/universal_robot/ur_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gents/ariac/src/universal_robot/ur_msgs
+# Build directory: /home/gents/ariac/build/universal_robot/ur_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
