4ca2afbfea88bda5f138ffccbaae88aef7096b6c
Gent Semaj <gentsemaj@live.com>
2020-02-05 00:34:09 -0500

    Removed build files

--- workspace/build/universal_robot/ur_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gents/ariac/src/universal_robot/ur_driver
-# Build directory: /home/gents/ariac/build/universal_robot/ur_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- workspace/build/universal_robot/ur_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gents/ariac/src/universal_robot/ur_msgs
-# Build directory: /home/gents/ariac/build/universal_robot/ur_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
