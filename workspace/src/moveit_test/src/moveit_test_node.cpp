a2be7399fee886e3d4682ea1ccda001b0f058c2a
Gent Semaj <gentsemaj@live.com>
2020-02-12 00:52:34 -0500

    Test node formal initialization

--- workspace/src/moveit_test/src/moveit_test_node.cpp
+++ workspace/src/moveit_test/src/moveit_test_node.cpp
@@ -17,11 +17,13 @@ int main(int argc, char** argv)
     // MoveIt! operates on sets of joints called "planning groups" and stores them in an object called
     // the `JointModelGroup`. Throughout MoveIt! the terms "planning group" and "joint model group"
     // are used interchangably.
-    static const std::string PLANNING_GROUP = "test_group";
+    static const std::string PLANNING_GROUP = "endeffector"; //THIS NEEDS TO BE SOMETHING SPECIFIC; LOOK AT THE SRDF FILE FOR GROUPS
 
     // The :move_group_interface:`MoveGroup` class can be easily
     // setup using just the name of the planning group you would like to control and plan for.
     moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+    
 
     ros::shutdown();
     return 0;
