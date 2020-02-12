2d774486f98c29586c076faa6c889ac0706b92d2
Gent Semaj <gentsemaj@live.com>
2020-02-11 21:56:37 -0500

    Bare bones roscpp integration
    -fixed dependencies
    -added custom package
    -added custom node in package

---
+++ workspace/src/moveit_test/src/moveit_test_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "moveit_test_node");
+    ros::NodeHandle node_handle;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    // BEGIN_TUTORIAL
+    //
+    // Setup
+    // ^^^^^
+    //
+    // MoveIt! operates on sets of joints called "planning groups" and stores them in an object called
+    // the `JointModelGroup`. Throughout MoveIt! the terms "planning group" and "joint model group"
+    // are used interchangably.
+    static const std::string PLANNING_GROUP = "test_group";
+
+    // The :move_group_interface:`MoveGroup` class can be easily
+    // setup using just the name of the planning group you would like to control and plan for.
+    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+    ros::shutdown();
+    return 0;
+}
